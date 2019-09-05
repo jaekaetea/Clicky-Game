7182fa91f42104ce4bfa5769280918386b37b07b
JoJo <jae.kae.tea@gmail.com>
2019-09-05 08:38:56 -0500

    Updates

--- precache-manifest.1f4ff1c4fab3fec5d0be0965a69a0949.js
+++ precache-manifest.1f4ff1c4fab3fec5d0be0965a69a0949.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "83a4ff28c15059561e548354ae9bdea3",
+    "revision": "8b838216c522933adbd777d232e4a0e9",
     "url": "/clicky-game/index.html"
   },
   {
-    "revision": "b545999a8638fae2cccb",
+    "revision": "8437dcabd0ef61a5515c",
     "url": "/clicky-game/static/css/main.d7236f43.chunk.css"
   },
   {
-    "revision": "ac94d7ed075f641fd23f",
-    "url": "/clicky-game/static/js/2.445b1b32.chunk.js"
+    "revision": "c781d8d14aa25daf4a98",
+    "url": "/clicky-game/static/js/2.0fb7f9dc.chunk.js"
   },
   {
-    "revision": "b545999a8638fae2cccb",
-    "url": "/clicky-game/static/js/main.f4a11c77.chunk.js"
+    "revision": "8437dcabd0ef61a5515c",
+    "url": "/clicky-game/static/js/main.1958e4bf.chunk.js"
   },
   {
     "revision": "79ea0d5502c3a6ac0f74",
@@ -22,9 +22,5 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "fd522166589d0fc9553f33db3776c22c",
     "url": "/clicky-game/static/media/background.fd522166.png"
-  },
-  {
-    "revision": "c2502c5a802b155ad40e577cf7e4d291",
-    "url": "/clicky-game/static/media/cat12.c2502c5a.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.3d11f14767843f3b236a89b8e889242d.js"
+  "/clicky-game/precache-manifest.1f4ff1c4fab3fec5d0be0965a69a0949.js"
 );
 
 self.addEventListener('message', (event) => {
