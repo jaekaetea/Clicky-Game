1f212ee2ab4d657a67a977d50b8b7d2c26242327
JoJo <jae.kae.tea@gmail.com>
2019-09-05 05:22:07 -0500

    Updates

---
+++ precache-manifest.cd4a67590be0b5de4ec3634b0b064b18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f72e6f5802a8d9d96420e1187fe4f6",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "cb81e74fb6020430f016",
+    "url": "/Clicky-Game/static/css/main.4d22eff6.chunk.css"
+  },
+  {
+    "revision": "ac94d7ed075f641fd23f",
+    "url": "/Clicky-Game/static/js/2.445b1b32.chunk.js"
+  },
+  {
+    "revision": "cb81e74fb6020430f016",
+    "url": "/Clicky-Game/static/js/main.09f9a710.chunk.js"
+  },
+  {
+    "revision": "75fa83e2a97bc1c69d72",
+    "url": "/Clicky-Game/static/js/runtime~main.4267543b.js"
+  },
+  {
+    "revision": "fd522166589d0fc9553f33db3776c22c",
+    "url": "/Clicky-Game/static/media/background.fd522166.png"
+  }
+]);
